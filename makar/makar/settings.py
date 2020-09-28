4935d24a008efdc8204999cca3977db9274e45ca
kurkutka <kurvvvvv5@gamil.com>
2020-09-29 01:10:45 +0300

    robiiiiiiit!!!!

--- makar/makar/settings.py
+++ makar/makar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'ddt'
 ]
 
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
